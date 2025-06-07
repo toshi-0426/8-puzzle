423548849642399c1206f94eeb5042606abd2546
Toshiyasu Takahashi <toshiyasutakahashi@Toshiyasus-MacBook-Air.local>
2025-06-07 15:33:03 +0900

    modified mode selector

--- src/components/ModeSelector.tsx
+++ src/components/ModeSelector.tsx
@@ -7,7 +7,22 @@ export function ModeSelector({ activeMode, onModeChange }: ModeSelectorProps) {
   return (
     <select
       value={activeMode}
-      className="border-2 rounded border-gray-900"
+      //className="border-2 rounded border-gray-900"
+      className="appearance-none 
+          bg-white 
+          border-2 
+          rounded-lg 
+          px-1 
+          py-1 
+          text-sm 
+          font-semibold 
+          text-gray-700 
+          focus:outline-none 
+          focus:ring-2 
+          focus:ring-blue-500 
+          focus:border-blue-500
+          cursor-pointer
+          "
       onChange={(e) => onModeChange(e.target.value as 'game' | 'solver')}
     >
       <option value="game" className="p-2 md:text-lg">
