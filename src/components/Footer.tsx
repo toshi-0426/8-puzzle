293da277977c6b8f27efe9016c4565b0871f7385
Toshiyasu Takahashi <t7316117@gmail.com>
2026-01-20 14:31:37 +0900

    fix: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="rounded-lg mt-4 flex justify-center">
       <div className="flex items-center text-gray-500 space-x-4">
-        <div>&copy; 2025 Toshiyasu Takahashi</div>
+        <div>&copy; 2026 Toshiyasu Takahashi</div>
         <a
           href="https://github.com/toshi-0426/8-puzzle"
           target="_blank"
