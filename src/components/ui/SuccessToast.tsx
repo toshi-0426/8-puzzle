ae0677247d584351a7690570a94686ee49654b8a
Toshiyasu Takahashi <t7316117@gmail.com>
2026-05-25 18:44:15 +0900

    style

--- src/components/ui/SuccessToast.tsx
+++ src/components/ui/SuccessToast.tsx
@@ -5,13 +5,21 @@ type SuccessToastProps = {
 
 function SuccessToast({ count, time }: SuccessToastProps) {
   return (
-    <div className="absolute inset-0 flex items-center justify-center bg-green-600/97 text-white rounded">
+    // <div className="absolute inset-0 flex items-center justify-center bg-green-600/97 text-white rounded">
+    <div
+      className="absolute inset-0 flex items-center justify-center bg-[#306230]/95 text-[#d9ead3] border-4 border-[#0f380f]
+  rounded"
+    >
       <div className="space-y-2">
         <div className="text-xl sm:text-3xl semi-bold font-mono text-center">
-          🎉 Congratulations 🎉
+          CLEAR!
         </div>
-        <div className="flex justify-center font-semibold">MOVES: {count}</div>
-        <div className="flex justify-center font-semibold">TIME: {time}</div>
+        <div className="flex justify-center font-semibold font-mono">
+          MOVES {count}
+        </div>
+        <div className="flex justify-center font-semibold font-mono">
+          TIME {time}
+        </div>
       </div>
     </div>
   );
